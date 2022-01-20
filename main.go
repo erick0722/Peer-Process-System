41357736b23f501a54cd48b8143aeb8d2ff34c3f
Erick <yiperick@hotmail.com>
2022-01-19 20:04:56 -0700

    Coded receivePeers

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	// servAddr := "localhost:6666"
 	servAddr := "localhost:55921"
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
@@ -24,49 +25,69 @@ func main() {
 		os.Exit(1)
 	}
 
-	reply := make([]byte, 1024)
+	for {
 
-	_, err = conn.Read(reply)
-	if err != nil {
-		println("Write to server failed:", err.Error())
-		os.Exit(1)
+		reply := make([]byte, 1024)
+
+		_, err = conn.Read(reply)
+		if err != nil {
+			println("Write to server failed:", err.Error())
+			os.Exit(1)
+		}
+
+		serverReply := string(bytes.Trim(reply, "\x00"))
+		var clientMessage string
+		println("reply from server=", serverReply)
+
+		switch {
+		case serverReply == "get team name\n":
+			clientMessage = "it takes two\n"
+			sendTeamName(clientMessage, conn)
+		case serverReply == "get code\n":
+			//TODO: send code over
+			clientMessage = "go\ndickass\n...\n"
+			sendTeamName(clientMessage, conn)
+		case serverReply == "receive peers\n":
+			clientMessage = "receive peers received\n"
+			//peerNum, peerList := receivePeers(conn)
+			_, _ = receivePeers(conn)
+		case serverReply == "get report\n":
+			clientMessage = "get report received\n"
+			// TODO
+		case serverReply == "close\n":
+			clientMessage = "bye have a great day\n"
+			conn.Close()
+		default:
+			clientMessage = "unknown message\n"
+		}
+
+		fmt.Printf("client message=%s", clientMessage)
 	}
-
-	serverReply := string(bytes.Trim(reply, "\x00"))
-	var clientMessage string
-	println("reply from server=", serverReply)
-
-	switch {
-	case serverReply == "get team name\n":
-		clientMessage = "it takes two\n"
-		sendTeamName(clientMessage, conn)
-	case serverReply == "get code\n":
-		clientMessage = "get code received\n"
-		// TODO
-	case serverReply == "receive peers\n":
-		clientMessage = "receive peers received\n"
-		// TODO
-	case serverReply == "get report\n":
-		clientMessage = "get report received\n"
-		// TODO
-	case serverReply == "close\n":
-		clientMessage = "bye have a great day\n"
-		conn.Close()
-	default:
-		clientMessage = "unknown message\n"
-	}
-
-	fmt.Printf("client message=%s", clientMessage)
-	conn.Close()
 }
 
-func sendTeamName(teamName string, conn net.Conn) () {
-	fmt.Printf("client message=%s", teamName)
-
+func sendTeamName(teamName string, conn net.Conn) {
 	_, err := conn.Write([]byte(teamName))
 	if err != nil {
 		print("Write to server failed:", err.Error())
 		os.Exit(1)
 	}
+}
 
+func receivePeers(conn net.Conn) (int, []string) {
+	reply := make([]byte, 1024)
+	_, err := conn.Read(reply)
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("reply from server=%s", reply)
+	numPeers, _ := strconv.Atoi(strings.Split(string(reply), "\n")[0])
+	fmt.Printf("numPeers=%d\n", numPeers)
+	peerIPs := make([]string, numPeers)
+	for i := 0; i < numPeers; i++ {
+		peerIPs[i] = strings.Split(string(reply), "\n")[i+1]
+		fmt.Printf("peerIPs[%d]=%s\n", i, peerIPs[i])
+	}
+
+	return numPeers, peerIPs
 }
