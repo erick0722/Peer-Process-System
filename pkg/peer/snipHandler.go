87f003914a29b598e6c1b3923d273c30393e07bf
Erick <yiperick@hotmail.com>
2022-03-03 17:22:09 -0700

    Added check for invalid snip

--- pkg/peer/snipHandler.go
+++ pkg/peer/snipHandler.go
@@ -11,16 +11,16 @@ import (
 	"time"
 )
 
-/** 
-* Reads a message from standard input to be sent to other peers. 
-* Users can send a message while our peer process reads messages, sends messages, etc. 
-* 
-* @param ctx {context.Context} 	The context initiated from the snipHandler. 
+/**
+* Reads a message from standard input to be sent to other peers.
+* Users can send a message while our peer process reads messages, sends messages, etc.
+*
+* @param ctx {context.Context} 	The context initiated from the snipHandler.
 * 							   	When the context's cancel function is called, will signal the
 *								function to gracefully exit
-*/
+ */
 func readSnip(ctx context.Context) {
-	// Read input from the user 
+	// Read input from the user
 	ch := make(chan string)
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
@@ -39,10 +39,10 @@ func readSnip(ctx context.Context) {
 	}
 }
 
-/** 
+/**
 * Send a snip from our peer process to another peer process(es)
 * @param input {string} The snip message read from standard input: contains correct formatting
-*/
+ */
 func sendSnip(input string) {
 	count := 0
 	currTimeStampStr := strconv.Itoa(currTimeStamp)
@@ -65,17 +65,21 @@ func sendSnip(input string) {
 	fmt.Printf("Sent [%s] to %d peers\n", input, count)
 }
 
-/** 
+/**
 * Store the sent snip in our own process.
 *
 * @param msg {string} The snip that was sent to our peer
-* @param source {string} The peer that sent the snip 
-*/
+* @param source {string} The peer that sent the snip
+ */
 func storeSnip(msg string, source string) {
 	message := strings.Split(msg, " ")
+	if len(message) < 2 {
+		fmt.Printf("Invalid message received: %s", msg)
+		return
+	}
 	snipList = append(snipList, snip{message[1], message[0], source})
-	
-	// Update the sender of the message in our peerlist 
+
+	// Update the sender of the message in our peerlist
 	index := searchPeerList(source)
 	if index != -1 {
 		peerList[index].lastHeard = time.Now()
@@ -89,8 +93,8 @@ func storeSnip(msg string, source string) {
 	fmt.Printf("Received %s from %s at timeStamp %s\n", snipList[len(snipList)-1].content, snipList[len(snipList)-1].source, snipList[len(snipList)-1].timeStamp)
 }
 
-/* 
-	Used to find max when implementing Lamport's Timestamp Algorithm for 
+/*
+	Used to find max when implementing Lamport's Timestamp Algorithm for
 
 	@param a {int} Integer 1
 	@param b {int} Integer 2
